0eb0f5d48e9fd5824ed2223d4b046aede306eeab
AIV <ilya_artemiev@icloud.com>
2026-02-01 13:10:27 +0300

    Delete src/X-bilder directory

--- src/X-bilder/bilder.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUILDER_H
-#define BUILDER_H
-
-void build__create_output   ( const char filename
-                            , char output_filename
-                            );
-
-#endif
