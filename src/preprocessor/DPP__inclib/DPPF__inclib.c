490c860cee519b7bf9ed5a3a553570ae7f9af306
AIV <ilya_artemiev@icloud.com>
2026-02-08 12:30:52 +0300

    Delete src/preprocessor/DPP__inclib directory

--- src/preprocessor/DPP__inclib/DPPF__inclib.c
+++
@@ -1,7 +0,0 @@
-#include "DPPF__inclib.h"
-#include <stdio.h>
-
-void DPPF__inclib(PreprocessorState* state, char* args) {
-    (void)state;
-    (void)args;
-}
--- src/preprocessor/DPP__inclib/DPPF__inclib.h
+++
@@ -1,8 +0,0 @@
-#ifndef DPPF__INCLIB_H
-#define DPPF__INCLIB_H
-
-#include "../preprocessor_state.h"
-
-void DPPF__inclib(PreprocessorState* state, char* args);
-
-#endif
