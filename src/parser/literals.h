64158d3f807e55c6a27df02b21575bfd57544dd1
AIV <ilya_artemiev@icloud.com>
2026-02-14 11:12:24 +0300

    Update literals.h

--- src/parser/literals.h
+++ src/parser/literals.h
@@ -9,5 +9,6 @@
 Token literal__parse_number(Lexer* lexer);
 Token literal__parse_char(Lexer* lexer);
 Token literal__parse_string(Lexer* lexer);
+Token literal__parse_concatenated(Lexer* lexer);
 
 #endif
