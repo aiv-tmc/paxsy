e663822ac2918383fdb1ff930ee3069fa17fd453
AIV <ilya_artemiev@icloud.com>
2026-02-14 11:09:44 +0300

    Delete src/preprocessor/DPP__define directory

--- src/preprocessor/DPP__define/DPPF__define.c
+++
@@ -1,7 +0,0 @@
-#include "DPPF__define.h"
-#include <stdio.h>
-
-void DPPF__define(PreprocessorState* state, char* args) {
-    (void)state;
-    (void)args;
-}
--- src/preprocessor/DPP__define/DPPF__define.h
+++
@@ -1,8 +0,0 @@
-#ifndef DPPF__define_H
-#define DPPF__define_H
-
-#include "../preprocessor_state.h"
-
-void DPPF__define(PreprocessorState* state, char* args);
-
-#endif
