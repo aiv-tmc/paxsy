cdc6d1a3f4f2349c99b1f6019ca8cb830648bd78
AIV <206456193+aiv-tmc@users.noreply.github.com>
2026-04-13 09:13:16 +0300

    Update include.h

--- src/preprocessor/directive/include/include.h
+++ src/preprocessor/directive/include/include.h
@@ -1,10 +1,22 @@
 #ifndef DIR__INCLUDE_H
 #define DIR__INCLUDE_H
 
+/* Forward declaration of the preprocessor state structure. */
 #include "../../preprocessor_state.h"
 
+/*
+ * Process the #import directive.
+ * Imports a header file (.hp) using a path relative to the current file.
+ * Prevents multiple inclusions via an internal registry.
+ */
 void DPPF__import(PreprocessorState* state, char* args);
 
+/*
+ * Process the #using directive.
+ * Imports a library header file (.hp) from standard system locations.
+ * Searches in: same directory as current file, current working directory,
+ * ./lib/, /usr/lib/paxsy/incl/, /usr/local/lib/paxsy/incl/.
+ */
 void DPPF__using(PreprocessorState* state, char* args);
 
 #endif
